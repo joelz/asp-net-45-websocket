4d04c6fa8af74aa39575c432457e77affc3fb831
Joel Zhang <rugoingdown@gmail.com>
2022-04-19 11:46:29 +0800

    add MSWSChat.sln

---
+++ MSWSChat/MSWSChatHandler.ashx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Web.WebSockets;
+
+namespace MSWSChat
+{
+    /// <summary>
+    /// Summary description for MSWSChatHandler
+    /// </summary>
+    public class MSWSChatHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest || context.IsWebSocketRequestUpgrading)
+            {
+                context.AcceptWebSocketRequest(new MyWSHandler());
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MSWSChat/MyWSHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Web.WebSockets;
+
+namespace MSWSChat
+{
+    public class MyWSHandler : WebSocketHandler
+    {
+        private static WebSocketCollection clients = new WebSocketCollection();
+
+        public override void OnOpen()
+        {
+            //this.Send("Welcom from " + this.WebSocketContext.UserHostAddress);
+            clients.Add(this);
+        }
+
+        public override void OnMessage(string message)
+        {
+            if (message == "ping")
+            {
+                this.Send("pong");
+            }
+            else if (message == "requestClose")
+            {
+                this.Close();
+            }
+            else
+            {
+                foreach (var c in clients)
+                {
+                    if (c != this && c.WebSocketContext.IsClientConnected)
+                    {
+                        c.Send(message);
+                    }
+                }
+            }
+        }
+
+        public override void OnClose()
+        {
+            clients.Remove(this);
+            base.OnClose();
+        }
+
+        public override void OnError()
+        {
+            base.OnError();
+        }
+    }
+}
